390febc89302c785f370299a5b4bbb3911a9799d
DucTrongVo <duc-trong.vo@toulouse.miage.fr>
2020-11-19 12:09:28 +0100

    first api

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,74 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./permissions.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://projetmobilite-a0b6f.firebaseio.com"
+});
+
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const db = admin.firestore();
+
+const cors = require('cors');
+app.use(cors( { origin: true } ));
 
 
+// Routes
+app.get("/hello-world", (req, res) => {
+    return res.status(200).send('Hello World');
+});
+
+
+// Create
+app.post('/api/users/create', (req,res) => {
+
+    (async () => {
+        try{
+            await db.collection('utilisateur').doc()
+            .create({
+                nom: req.body.nom,
+                prenom: req.body.prenom,
+                mail: req.body.mail,
+                themes: req.body.themes
+            });
+
+            return res.status(200).send();
+        }catch(error) {
+            console.log("Error : ",error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+
+// Read
+
+// get user by id
+app.get('/api/users/:id', (req,res) => {
+
+    (async () => {
+        try{
+            const doc = db.collection('utilisateur').doc(req.params.id); 
+            let user = await doc.get();
+            let response = user.data();
+            
+            return res.status(200).send(response);
+        }catch(error) {
+            console.log("Error : ",error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+// Update
+
+
+// Delete
+
+
+
+// Export the api to Firebase
+exports.app = functions.https.onRequest(app);
